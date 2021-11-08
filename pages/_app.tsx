35e1d0a114b5fefa14ea205d2acb2416f73be27d
manot40 <kevinsandiho@outlook.com>
2021-11-08 21:36:37 +0700

    Added Base NextUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CssBaseline } from "@nextui-org/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default App;
