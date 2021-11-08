8a2137b9ec3c836b119b3a6defcc80d9471ded77
manot40 <kevinsandiho290100@gmail.com>
2021-11-08 23:59:08 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,6 @@ export default function Home() {
         as="div"
         display="flex"
         direction="column"
-        justify="center"
         alignItems="center"
         style={{ height: "100vh" }}
       >
