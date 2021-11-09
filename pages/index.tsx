61b0ab3c727db1f1e4c7160c8257b0360f19ed15
manot40 <kevinsandiho@outlook.com>
2021-11-09 07:06:17 +0700

    Tidying up

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,8 @@ export default function Home() {
         as="div"
         display="flex"
         direction="column"
-        justify="center"
         alignItems="center"
-        style={{ height: "100vh" }}
+        style={{ marginTop: "15vh" }}
       >
         <Avatar
           src="/default.jpg"
@@ -47,19 +46,26 @@ export default function Home() {
           />
           <Spacer />
           <Container
-           as="div"
-           display="flex"
-           direction="row"
-           justify="space-between">
-             <Checkbox size="small" label="Ingat saya" checked />
-             <Link color underline href="#">
-               Lupa password?
-             </Link>
+            as="div"
+            display="flex"
+            direction="row"
+            justify="space-between"
+          >
+            <Checkbox size="small" label="Ingat saya" checked />
+            <Link color underline href="#">
+              Lupa password?
+            </Link>
           </Container>
           <Spacer y={2} />
-          <Button shadow color="gradient" style={{ width: 18 + 'rem' }}>
+          <Button shadow color="gradient" style={{ width: 18 + "rem" }}>
             Login
           </Button>
+          <Spacer y={1.5} />
+          <Container as="div" display="flex" direction="row" justify="center">
+            <Link color="#333" underline href="#">
+              Tidak memiliki akun? Registrasi
+            </Link>
+          </Container>
         </form>
       </Container>
     </div>
