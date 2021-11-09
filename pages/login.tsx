580cf7a0611026875d65000a0192bf170725d659
manot40 <kevinsandiho290100@gmail.com>
2021-11-09 12:54:31 +0700

    master

--- pages/login.tsx
+++ pages/login.tsx
@@ -12,6 +12,8 @@ import styles from "../styles/Home.module.css";
 import { MailIcon, LockIcon } from "@primer/octicons-react";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isError, setIsError] = useState(false);
   const [pwStrong, setPwStrong] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -20,8 +22,24 @@ export default function Login() {
     console.log("placeholder");
     return () => {
       setIsLoading(false);
-    }
+    };
   }, []);
+
+  function verifyPwInput(pw: string) {
+    setPassword(pw);
+    const criteria = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+    );
+    const isStrong = pw.match(criteria);
+    if (pw !== "") isStrong?.length ? setPwStrong(true) : setPwStrong(false);
+    else setPwStrong(true);
+  }
+  function onError() {
+    setIsError(true);
+    setTimeout(() => {
+      setIsError(false);
+    }, 1500);
+  }
 
   return (
     <div className={styles.container}>
@@ -42,10 +60,12 @@ export default function Login() {
         <Spacer y={2} />
         <form>
           <Input
+            onChange={(e) => setEmail(e.target.value)}
             helperColor={isError ? "error" : "default"}
             status={isError ? "error" : "default"}
             contentLeft={<MailIcon size={16} />}
             type="email"
+            value={email}
             width="18rem"
             placeholder="Email"
             clearable
@@ -57,14 +77,16 @@ export default function Login() {
                 ? "Harus memiliki huruf, angka, simbol dan huruf besar"
                 : ""
             }
+            onChange={(e) => verifyPwInput(e.target.value)}
             helperColor={isError || !pwStrong ? "error" : "default"}
             status={isError || !pwStrong ? "error" : "default"}
             contentLeft={<LockIcon size={16} />}
+            value={password}
             width="18rem"
             placeholder="Password"
             clearable
           />
-          <Spacer />
+          <Spacer y={!pwStrong ? 1.5 : 1} />
           <Container
             as="div"
             display="flex"
@@ -87,7 +109,7 @@ export default function Login() {
           </Button>
         </form>
         <Spacer y={2} />
-        <Link color="#333" underline href="#">
+        <Link color="#333" underline onClick={() => onError()}>
           Belum punya akun? Registrasi
         </Link>
       </Container>
