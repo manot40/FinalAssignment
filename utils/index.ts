92b56b07b57573f186cd6c75651ada0d104e31b3
manot40 <kevinsandiho@outlook.com>
2021-11-12 17:09:55 +0700

    Simplify Password RegEx

--- utils/index.ts
+++ utils/index.ts
@@ -17,7 +17,7 @@ export function hashDecode(window: Window) {
 
 export function isEmailValid(email: string) {
   const isValid = email.match(
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})$/
   );
   return typeof isValid?.length !== "undefined";
 }
